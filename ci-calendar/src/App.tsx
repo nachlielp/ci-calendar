6ae6c74f6dd3a1542af2717b2f9e93d905d283b7
nachlielEasy <33023571+nachpok@users.noreply.github.com>
2024-04-13 03:47:24 +0300

    Update App.tsx

--- ci-calendar/src/App.tsx
+++ ci-calendar/src/App.tsx
@@ -43,6 +43,7 @@ export default function App() {
               <Route element={<PrivateRoutes />}></Route>
 
               {/* Teacher privet routes */}
+
               <Route
                 element={
                   <PrivateRoutes
