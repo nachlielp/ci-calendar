bbe449828bc87e5d6a10d5d6c627d1e8b5c0eaec
nachlielEasy <33023571+nachpok@users.noreply.github.com>
2024-04-18 16:06:33 +0300

    WIP calendar - missing read mode in params

--- ci-calendar/src/Components/UI/CalendarView.tsx
+++ ci-calendar/src/Components/UI/CalendarView.tsx
@@ -2,65 +2,56 @@ import type { CalendarProps } from "antd";
 import { Calendar, Card } from "antd";
 import type { Dayjs } from "dayjs";
 import { useWindowSize } from "../../hooks/useWindowSize";
+import { IEvently } from "../../util/interfaces";
+import dayjs from "dayjs";
 
-const getListData = (value: Dayjs) => {
-  let listData;
-  switch (value.date()) {
-    case 8:
-      listData = [{ type: "warning" }];
-      break;
-    case 10:
-      listData = [{ type: "warning" }];
-      break;
-    case 15:
-      listData = [{ type: "warning" }];
-      break;
-    default:
-  }
-  return listData || [];
-};
+interface CalendarViewProps {
+  events: IEvently[];
+  selectedDay: Dayjs;
+  onSelect: (value: Dayjs) => void;
+}
 
-const getMonthData = (value: Dayjs) => {
-  if (value.month() === 8) {
-    return 1394;
-  }
-};
-
-export default function CalendarView() {
+export default function CalendarView({
+  events,
+  selectedDay,
+  onSelect,
+}: CalendarViewProps) {
   const { width } = useWindowSize();
   const setWidth = width > 600 ? 500 : width * 0.9;
-  const monthCellRender = (value: Dayjs) => {
-    const num = getMonthData(value);
-    return num ? (
-      <div className="notes-month">
-        <section>{num}</section>
-        <span>Backlog number</span>
-      </div>
-    ) : null;
-  };
+
   const dateCellRender = (value: Dayjs) => {
-    const listData = getListData(value);
-    const hasEvents = listData.length > 0;
     return (
       <div className="day-cell p-2 relative">
-        {hasEvents && (
+        {eventsOnDay(value, events) && (
           <span className="absolute bottom-1 right-1 mr-[3px] h-2 w-2 bg-blue-500 rounded-full"></span>
-          // Using Tailwind's negative margin utility `mr-[-2px]` to move the dot 2px to the left
         )}
       </div>
     );
   };
+  //Render each cell in the calendar
   const cellRender: CalendarProps<Dayjs>["cellRender"] = (current, info) => {
     if (info.type === "date") return dateCellRender(current);
-    if (info.type === "month") return monthCellRender(current);
     return info.originNode;
   };
 
   return (
     <div style={{ width: setWidth }} className="mx-auto">
       <Card className="mx-auto mt-5">
-        <Calendar fullscreen={false} cellRender={cellRender} />
+        <Calendar
+          fullscreen={false}
+          cellRender={cellRender}
+          mode="month"
+          onSelect={onSelect}
+        />
       </Card>
     </div>
+  );
+}
+
+function eventsOnDay(day: Dayjs, events: IEvently[]) {
+  return (
+    events.filter((event) =>
+      dayjs(event.subEvents[0].startTime).isSame(day, "day")
+    ).length > 0
   );
 }
--- ci-calendar/src/Components/UI/EventsDisplay.tsx
+++ ci-calendar/src/Components/UI/EventsDisplay.tsx
@@ -1,6 +1,8 @@
+import dayjs, { Dayjs } from "dayjs";
 import { IEvently } from "../../util/interfaces";
 import CalendarView from "./CalendarView";
 import EventsList from "./EventsList";
+import { useState } from "react";
 
 interface IEventsDisplayProps {
   events: IEvently[];
@@ -8,10 +10,30 @@ interface IEventsDisplayProps {
 }
 
 function EventsDisplay({ events, isEdit }: IEventsDisplayProps) {
+  const [selectedDay, setSelectedDay] = useState<Dayjs>(dayjs());
+  const onSelect = (value: Dayjs) => {
+    setSelectedDay(value);
+  };
+  const todaysEvents = events.filter((event) =>
+    dayjs(event.subEvents[0].startTime).isSame(selectedDay, "day")
+  );
+
   const flag = true;
+
   return (
     <>
-      {flag ? <CalendarView /> : <EventsList events={events} isEdit={isEdit} />}
+      {flag ? (
+        <>
+          <CalendarView
+            events={events}
+            selectedDay={selectedDay}
+            onSelect={onSelect}
+          />
+          <EventsList events={todaysEvents} isEdit={isEdit} />
+        </>
+      ) : (
+        <EventsList events={events} isEdit={isEdit} />
+      )}
     </>
   );
 }
