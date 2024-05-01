dcfe346ff4f0f20e8162f6672455ade92313e47e
nachlielEasy <33023571+nachpok@users.noreply.github.com>
2024-05-01 18:26:31 +0300

    Repeating events

--- ci-calendar/src/Components/UI/EventForm.tsx
+++ ci-calendar/src/Components/UI/EventForm.tsx
@@ -8,12 +8,20 @@ import {
   Form,
   Input,
   InputNumber,
+  List,
   Row,
   Select,
   TimePicker,
+  Tooltip,
 } from "antd";
 import customParseFormat from "dayjs/plugin/customParseFormat";
-import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import {
+  InfoCircleOutlined,
+  MinusCircleOutlined,
+  PlusOutlined,
+} from "@ant-design/icons";
+import VirtualList from "rc-virtual-list";
+
 import { v4 as uuidv4 } from "uuid";
 
 import dayjs from "dayjs";
@@ -46,6 +54,9 @@ const initialValues = {
 };
 
 export default function EventForm() {
+  const [repeatOption, setRepeatOption] = useState<Frequency>(Frequency.none);
+  const [eventDate, setEventDate] = useState(dayjs());
+  const [endDate, setEndDate] = useState<dayjs.Dayjs | null>(null);
   const navigate = useNavigate();
   const { currentUser, createEvent } = useAuthContext();
   const [address, setAddress] = useState<IAddress>();
@@ -69,50 +80,110 @@ export default function EventForm() {
     setAddress(selectedAddress);
     form.setFieldValue("address", selectedAddress);
   };
+
+  const handleRepeatChange = () => {
+    setRepeatOption(form.getFieldValue("event-repeat"));
+  };
+
+  const handleDateChange = (date: dayjs.Dayjs) => {
+    setEventDate(date);
+  };
+
+  const handleEndDateChange = (date: dayjs.Dayjs) => {
+    setEndDate(date);
+  };
+
   const handleSubmit = async (values: any) => {
-    const subEvents = [
+    console.log("EventForm.handleSubmit.values: ", values);
+    const subEventsTemplate = [
       {
-        startTime: dayjs(values["event-time"][0]).toISOString(),
-        endTime: dayjs(values["event-time"][1]).toISOString(),
+        startTime: dayjs(values["event-time"][0]),
+        endTime: dayjs(values["event-time"][1]),
         type: values["event-types"],
         tags: values["event-tags"] || [],
         teacher: values["event-teacher"] || "",
       },
     ];
+
     if (values["sub-events"]) {
       values["sub-events"].forEach((subEvent: any) =>
-        subEvents.push({
+        subEventsTemplate.push({
           type: subEvent.type,
           tags: subEvent.tags || [],
           teacher: subEvent.teacher || "",
-          startTime: dayjs(subEvent.time[0]).toISOString(),
-          endTime: dayjs(subEvent.time[1]).toISOString(),
+          startTime: dayjs(subEvent.time[0]),
+          endTime: dayjs(subEvent.time[1]),
         })
       );
     }
+
     if (!address) {
       return;
     }
-    console.log("EventForm.handleSubmit.address: ", address);
-    const event: IEvently = {
-      id: uuidv4(),
-      address: address,
-      createdAt: dayjs().toISOString(),
-      updatedAt: dayjs().toISOString(),
-      title: values["event-title"],
-      description: values["event-description"] || "",
-      owners: [currentUser.id],
-      links: values["links"] || [],
-      price: values["prices"] || [],
-      hide: false,
-      subEvents: subEvents,
-      district: values["district"],
-    };
+
     try {
-      await createEvent(event);
+      if (repeatOption === Frequency.none) {
+        const event: IEvently = {
+          id: uuidv4(),
+          address: address,
+          createdAt: dayjs().toISOString(),
+          updatedAt: dayjs().toISOString(),
+          title: values["event-title"],
+          description: values["event-description"] || "",
+          owners: [currentUser.id],
+          links: values["links"] || [],
+          price: values["prices"] || [],
+          hide: false,
+          subEvents: subEventsTemplate.map((subEvent) => ({
+            ...subEvent,
+            startTime: subEvent.startTime.toISOString(),
+            endTime: subEvent.endTime.toISOString(),
+          })),
+          district: values["district"],
+        };
+        await createEvent(event);
+      } else if (endDate) {
+        const dates = listOfDates(
+          eventDate,
+          endDate,
+          repeatOption,
+          form.getFieldValue("event-repeat-week-interval")
+        );
+
+        for (const date of dates) {
+          const subEvents = subEventsTemplate.map((subEvent) => ({
+            ...subEvent,
+            startTime: date
+              .hour(subEvent.startTime.hour())
+              .minute(subEvent.startTime.minute())
+              .toISOString(),
+            endTime: date
+              .hour(subEvent.endTime.hour())
+              .minute(subEvent.endTime.minute())
+              .toISOString(),
+          }));
+
+          const event: IEvently = {
+            id: uuidv4(),
+            address: address,
+            createdAt: dayjs().toISOString(),
+            updatedAt: dayjs().toISOString(),
+            title: values["event-title"],
+            description: values["event-description"] || "",
+            owners: [currentUser.id],
+            links: values["links"] || [],
+            price: values["prices"] || [],
+            hide: false,
+            subEvents: subEvents,
+            district: values["district"],
+          };
+          await createEvent(event);
+        }
+      }
       navigate("/");
     } catch (error) {
       console.error("EventForm.handleSubmit.error: ", error);
+      throw error;
     }
   };
 
@@ -172,8 +243,94 @@ export default function EventForm() {
               format={"DD/MM"}
               minDate={dayjs()}
               maxDate={dayjs().add(1, "year")}
+              onChange={handleDateChange}
             />
           </Form.Item>
+
+          <Form.Item
+            className="mt-4"
+            label={
+              <Tooltip title={repeatTooltip}>
+                חזרה &nbsp;
+                <InfoCircleOutlined />
+              </Tooltip>
+            }
+            name="event-repeat"
+            rules={[{ required: true, message: "שדה חובה" }]}
+          >
+            <Select
+              options={repeatOptions}
+              defaultValue={Frequency.none}
+              onChange={handleRepeatChange}
+            />
+          </Form.Item>
+
+          {repeatOption === Frequency.byWeek && (
+            <Form.Item
+              label="שבועות"
+              name="event-repeat-week-interval"
+              className="mt-4"
+              rules={[{ required: true, message: "שדה חובה" }]}
+            >
+              <InputNumber min={1} precision={0} />
+            </Form.Item>
+          )}
+          {repeatOption === Frequency.monthly && (
+            <Form.Item
+              label="תדירות"
+              name="event-repeat-week-frequency"
+              className="mt-4"
+            >
+              <Input placeholder={formatMonthlyDate(eventDate)} disabled />
+            </Form.Item>
+          )}
+          {repeatOption !== "none" && (
+            <>
+              <Form.Item
+                label="סיום חזרה"
+                name="event-repeat-end-date"
+                className="mt-4"
+                rules={[{ required: true, message: "שדה חובה" }]}
+              >
+                <DatePicker
+                  format={"DD/MM"}
+                  minDate={eventDate}
+                  maxDate={dayjs().add(1, "year")}
+                  onChange={handleEndDateChange}
+                />
+              </Form.Item>
+              <Form.Item
+                label="תאריכים"
+                name="event-list-of-dates"
+                className="mt-4"
+              >
+                {endDate && (
+                  <List>
+                    <VirtualList
+                      data={listOfDates(
+                        eventDate,
+                        endDate,
+                        repeatOption,
+                        form.getFieldValue("event-repeat-week-interval")
+                      )}
+                      height={200}
+                      itemHeight={47}
+                      itemKey="email"
+                    >
+                      {(date: dayjs.Dayjs, index: number) => (
+                        <List.Item key={index} className="mr-4">
+                          <List.Item.Meta
+                            key={index}
+                            title={date.format("DD/MM")}
+                          />
+                        </List.Item>
+                      )}
+                    </VirtualList>
+                  </List>
+                )}
+              </Form.Item>
+            </>
+          )}
         </Card>
 
         <Card className="mt-4 border-2">
@@ -424,3 +581,126 @@ export default function EventForm() {
     </Card>
   );
 }
+
+enum Frequency {
+  none = "none",
+  weekly = "weekly",
+  byWeek = "by-week",
+  monthly = "monthly",
+}
+
+const repeatOptions = [
+  { value: Frequency.none, label: "אף פעם" },
+  { value: Frequency.weekly, label: "כל  שבוע" },
+  { value: Frequency.byWeek, label: "כל כמה שבועות" },
+  { value: Frequency.monthly, label: "כל חודש" },
+];
+
+const repeatTooltip = (
+  <>
+    <p>* כל כמה שבועות - לדוגמה, כל שבועים ביום שלישי </p>
+    <br />
+    <p>* כל חודש - לדוגמה, השבת השניה של כל חודש</p>
+  </>
+);
+
+function getDayAndWeekOfMonth(date: dayjs.Dayjs) {
+  const dayOfWeek = date.day(); // 0 (Sunday) to 6 (Saturday)
+  const dayOfMonth = date.date();
+  const weekOfMonth = Math.ceil(dayOfMonth / 7);
+
+  return { dayOfWeek, weekOfMonth };
+}
+
+function listOfDates(
+  startDate: dayjs.Dayjs,
+  endDate: dayjs.Dayjs,
+  repeatOption: Frequency,
+  repeatInterval?: number
+) {
+  const dates = [];
+  let date = startDate;
+  if (repeatOption === Frequency.weekly) {
+    while (!date.isAfter(endDate.add(1, "day"))) {
+      dates.push(date);
+      date = date.add(1, "week");
+    }
+  } else if (repeatOption === Frequency.byWeek && repeatInterval) {
+    while (!date.isAfter(endDate.add(1, "day"))) {
+      dates.push(date);
+      date = date.add(repeatInterval, "week");
+    }
+  } else if (repeatOption === Frequency.monthly) {
+    const { dayOfWeek, weekOfMonth } = getDayAndWeekOfMonth(startDate);
+    while (!date.isAfter(endDate)) {
+      dates.push(date);
+      date = date.add(1, "month");
+      date = moveToSameWeekAndDay(date, dayOfWeek, weekOfMonth);
+    }
+  }
+  return dates;
+}
+
+function moveToSameWeekAndDay(
+  date: dayjs.Dayjs,
+  dayOfWeek: number,
+  weekOfMonth: number
+): dayjs.Dayjs {
+  const monthStart = date.startOf("month");
+  let adjustedDate = monthStart.add((weekOfMonth - 1) * 7, "day");
+  while (adjustedDate.day() !== dayOfWeek) {
+    adjustedDate = adjustedDate.add(1, "day");
+  }
+  if (adjustedDate.month() !== monthStart.month()) {
+    adjustedDate = adjustedDate.subtract(1, "week");
+  }
+  return adjustedDate;
+}
+
+const formatMonthlyDate = (date: dayjs.Dayjs) => {
+  const { dayOfWeek, weekOfMonth } = getDayAndWeekOfMonth(date);
+  let day;
+  switch (dayOfWeek) {
+    case 0:
+      day = "ראשון";
+      break;
+    case 1:
+      day = "שני";
+      break;
+    case 2:
+      day = "שלישי";
+      break;
+    case 3:
+      day = "רביעי";
+      break;
+    case 4:
+      day = "חמישי";
+      break;
+    case 5:
+      day = "שישי";
+      break;
+    case 6:
+      day = "שבת";
+      break;
+  }
+
+  let frequency;
+  switch (weekOfMonth) {
+    case 1:
+      frequency = "ראשונה";
+      break;
+    case 2:
+      frequency = "שניה";
+      break;
+    case 3:
+      frequency = "שלישית";
+      break;
+    case 4:
+      frequency = "רביעית";
+      break;
+    default:
+      frequency = "חמישית";
+      break;
+  }
+  return `יום ${day} ה${frequency} בחודש`;
+};
