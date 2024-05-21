a5197017c71ce2f71f12f829ceb68bc95ef3ec32
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-05-21 20:44:04 +0300

    Add android supprot to open address

--- ci-calendar/src/Components/UI/DisplayEvents/SingleDayEventCard.tsx
+++ ci-calendar/src/Components/UI/DisplayEvents/SingleDayEventCard.tsx
@@ -27,18 +27,23 @@ export const SingleDayEventCard = React.forwardRef<
 
   function openGoogleMaps(placeId: string, address: string) {
     const iosUrl = `comgooglemaps://?q=${encodeURIComponent(address)}`;
+    const androidUrl = `geo:0,0?q=${encodeURIComponent(address)}`;
     const fallbackUrl = `https://www.google.com/maps/place/?q=place_id:${placeId}`;
+
     if (/(iPhone|iPad|iPod)/.test(navigator.userAgent)) {
       setTimeout(function () {
         window.location.href = fallbackUrl;
       }, 25);
       window.open(iosUrl, '_blank');
-      // window.location.href = iosUrl;
+    } else if (/Android/.test(navigator.userAgent)) {
+      setTimeout(function () {
+        window.location.href = fallbackUrl;
+      }, 25);
+      window.open(androidUrl, '_blank');
     } else {
       window.open(fallbackUrl, '_blank');
     }
   }
-
   const subEventLen = Object.values(event.subEvents).length;
   const teachersIds = getEventTeachersIds(event);
   const { teachers } = useTeacherBio({ ids: teachersIds });
