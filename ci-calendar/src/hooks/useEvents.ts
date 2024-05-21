7463116242ae03077da9fa9c951e2142dc32f79c
Nachliel <33023571+nachpok@users.noreply.github.com>
2024-05-21 17:27:55 +0300

    Sort events at source

--- ci-calendar/src/hooks/useEvents.ts
+++ ci-calendar/src/hooks/useEvents.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { firebaseService } from "../firebase.service";
 import { IEvently } from "../util/interfaces";
+import dayjs from "dayjs";
 
 export const useEvents = () => {
   const [events, setEvents] = useState<IEvently[]>([]);
@@ -10,7 +11,8 @@ export const useEvents = () => {
     const initTFirebase = async () => {
       firebaseService.initFirebaseJS();
       firebaseService.subscribeToCollection("events", (events: IEvently[]) => {
-        setEvents(events);
+        const sortedEvents = events.sort((a, b) => dayjs(a.dates["startDate"]).diff(dayjs(b.dates["startDate"])));
+        setEvents(sortedEvents);
         setLoading(false);
       });
     };
