3a49cb39a61803420071270b7692e3fe55b92406
Nachliel <nachpok@gmail.com>
2024-05-28 01:06:47 +0300

    responsive teacher and admin pages

--- ci-calendar/src/Components/Pages/AdminPage.tsx
+++ ci-calendar/src/Components/Pages/AdminPage.tsx
@@ -3,21 +3,27 @@ import { LinkButton } from "../UI/Other/LinkButton";
 function AdminPage() {
   return (
     <div className="flex flex-col sm:flex-row items-center justify-center mt-10">
-      <LinkButton to="/bio" className="w-24 h-24 m-4">
-        פרופיל אישי
-      </LinkButton>
-      <LinkButton to="/manage-events" className="w-24 h-24 m-4">
-        עריכת אירועים
-      </LinkButton>
-      <LinkButton to="/event-form" className="w-24 h-24 m-4">
-        הוסף אירוע <br /> חד יומי
-      </LinkButton>
-      <LinkButton to="/multi-day-event-form" className="w-24 h-24 m-4">
-        הוסף ארוע <br /> רב יומי
-      </LinkButton>
-      <LinkButton to="/manage-users" className="w-24 h-24 m-4">
-        הגדרת <br /> משתמשים
-      </LinkButton>
+      <div className="flex flex-row items-center justify-center">
+        <LinkButton to="/bio" className="w-24 h-24 m-4">
+          פרופיל אישי
+        </LinkButton>
+        <LinkButton to="/manage-events" className="w-24 h-24 m-4">
+          עריכת אירועים
+        </LinkButton>
+      </div>
+      <div className="flex flex-row items-center justify-center">
+        <LinkButton to="/event-form" className="w-24 h-24 m-4">
+          הוסף אירוע <br /> חד יומי
+        </LinkButton>
+        <LinkButton to="/multi-day-event-form" className="w-24 h-24 m-4">
+          הוסף ארוע <br /> רב יומי
+        </LinkButton>
+      </div>
+      <div className="flex flex-row items-center justify-center">
+        <LinkButton to="/manage-users" className="w-24 h-24 m-4">
+          הגדרת <br /> משתמשים
+        </LinkButton>
+      </div>
     </div>
   );
 }
--- ci-calendar/src/Components/Pages/TeacherPage.tsx
+++ ci-calendar/src/Components/Pages/TeacherPage.tsx
@@ -3,21 +3,22 @@ import { LinkButton } from "../UI/Other/LinkButton";
 function TeacherPage() {
   return (
     <div className="flex flex-col sm:flex-row items-center justify-center mt-10">
-      <LinkButton to="/bio" className="w-24 h-24 m-4">
-        פרופיל אישי
-      </LinkButton>
-      {/* <LinkButton to="/edit-events-list" className="w-24 h-24 m-4">
-        הארועים שלי
-      </LinkButton> */}
-      <LinkButton to="/manage-events" className="w-24 h-24 m-4">
-        הארועים שלי
-      </LinkButton>
-      <LinkButton to="/event-form" className="w-24 h-24 m-4">
-        הוסף אירוע <br /> חד יומי
-      </LinkButton>
-      <LinkButton to="/multi-day-event-form" className="w-24 h-24 m-4">
-        הוסף ארוע <br /> רב יומי
-      </LinkButton>
+      <div className="flex flex-row items-center justify-center">
+        <LinkButton to="/bio" className="w-24 h-24 m-4">
+          פרופיל אישי
+        </LinkButton>
+        <LinkButton to="/manage-events" className="w-24 h-24 m-4">
+          הארועים שלי
+        </LinkButton>
+      </div>
+      <div className="flex flex-row items-center justify-center">
+        <LinkButton to="/event-form" className="w-24 h-24 m-4">
+          הוסף אירוע <br /> חד יומי
+        </LinkButton>
+        <LinkButton to="/multi-day-event-form" className="w-24 h-24 m-4">
+          הוסף ארוע <br /> רב יומי
+        </LinkButton>
+      </div>
     </div>
   );
 }
