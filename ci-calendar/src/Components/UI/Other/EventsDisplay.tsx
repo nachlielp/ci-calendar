25503f546254dd0e5dd50fd43a635b6795a485ba
Nachliel <nachpok@gmail.com>
2024-06-01 18:21:26 +0300

    replace view calendar by params with state

--- ci-calendar/src/Components/UI/Other/EventsDisplay.tsx
+++ ci-calendar/src/Components/UI/Other/EventsDisplay.tsx
@@ -6,12 +6,10 @@ import { IEvently } from "../../../util/interfaces";
 import CalendarView from "./CalendarView";
 import EventsList from "../DisplayEvents/EventsList";
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
 import { useEventsFilter } from "../../../hooks/useEventsFilter";
 import FilterModel from "./FilterModel";
 import { Button } from "antd";
-import { viewOptions } from "../../../util/options";
-import { useParamsHandler } from "../../../hooks/useParamsHandler";
+
 import { Icon } from "./Icon";
 
 interface IEventsDisplayProps {
@@ -22,21 +20,15 @@ interface IEventsDisplayProps {
 //TODO hide scroll base on list view
 function EventsDisplay({ events, isEdit }: IEventsDisplayProps) {
   const [futureEvents, setFutureEvents] = useState<IEvently[]>([]);
-  const {
-    currentValues: viewCurrentValues,
-    onOptionsChange,
-    clearSearchParams,
-  } = useParamsHandler({ title: "view", options: viewOptions });
+  const [isListView, setIsListView] = useState<boolean>(true);
   useEffect(() => {
     const futureEvents = events.filter((event) => dayjs(event.dates["endDate"]).isAfter(dayjs().startOf('day')));
     setFutureEvents(futureEvents);
   }, [events]);
 
   const [selectedDay, setSelectedDay] = useState<Dayjs>(dayjs());
-
   const [todaysEvents, setTodaysEvents] = useState<IEvently[]>([]);
-  const [searchParams] = useSearchParams();
-  const view = searchParams.get("view");
+
   const onSelect = (value: Dayjs) => {
     setSelectedDay(value);
   };
@@ -50,14 +42,15 @@ function EventsDisplay({ events, isEdit }: IEventsDisplayProps) {
     setTodaysEvents(todaysEvents);
   }, [selectedDay]);
 
-  const handleViewChange = () => {
-    if (viewCurrentValues.length === 0) {
-      setSelectedDay(dayjs())
-      onOptionsChange("view")(["calendar"]);
-    } else {
-      clearSearchParams(["view"]);
-    }
-  };
+  function onViewList() {
+    setIsListView(true);
+  }
+
+  function onViewCalendar() {
+    setIsListView(false);
+    setSelectedDay(dayjs())
+  }
+
   return (
     <>
       <header className="flex flex-col justify-center items-center mt-5">
@@ -66,21 +59,21 @@ function EventsDisplay({ events, isEdit }: IEventsDisplayProps) {
           <FilterModel />
           <div className="flex flex-row">
             <Button
-              onClick={handleViewChange}
-              className={`flex items-center justify-center  rounded-r-none  ${!viewCurrentValues.length ? 'bg-gray-300' : ''}`}
+              onClick={onViewList}
+              className={`flex items-center justify-center  rounded-r-none  ${isListView ? 'bg-gray-300' : ''}`}
             >
               <Icon icon="viewDay" className="w-6 h-6 " />
             </Button>
             <Button
-              onClick={handleViewChange}
-              className={`flex items-center justify-center  rounded-l-none  ${viewCurrentValues.length ? 'bg-gray-300' : ''}`}
+              onClick={onViewCalendar}
+              className={`flex items-center justify-center  rounded-l-none  ${!isListView ? 'bg-gray-300' : ''}`}
             >
               <Icon icon="calendar" className="w-6 h-6 " />
             </Button>
           </div>
         </main>
       </header>
-      {view === "calendar" ? (
+      {!isListView ? (
         <>
           <CalendarView
             events={filteredEvents}
