ad6b5ca3a5afe99d2f7404f1b34623d6d0666dfb
Nachliel <nachpok@gmail.com>
2024-06-18 08:17:29 +0300

    style bio page

--- ci-calendar/src/Components/UI/DisplayUsers/BioCard.tsx
+++ ci-calendar/src/Components/UI/DisplayUsers/BioCard.tsx
@@ -1,50 +1,57 @@
 import { Button, Card, Image } from "antd";
 import { useAuthContext } from "../../Auth/AuthContext";
-import Meta from "antd/es/card/Meta";
 import { useGetTeacher } from "../../../hooks/useGetTeacher";
+import Title from "antd/es/typography/Title";
+import { Typography } from 'antd';
+const { Text } = Typography;
 
 function BioCard() {
   const { currentUser } = useAuthContext();
   if (!currentUser) throw new Error("BioCard: No user found");
   const teacher = useGetTeacher(currentUser.id);
 
-
   const imgComponent = teacher?.img ? (
     <Image
       alt="example"
       src={teacher.img}
       key={teacher.img}
+      preview={false}
       width={200}
       height={200}
-      style={{ objectFit: "cover" }}
-      preview={false}
+      className="bio-card-img"
     />
   ) : (
     ""
   );
 
   return (
-    <div className="bio-card">
-      {teacher && <Card
-        // className=" mt-6  w-full"
-        hoverable
-        style={{ width: 200 }}
-      >
-        {imgComponent}
-        <Meta title={teacher.fullName} description={teacher.bio} />
-        {teacher.pageUrl && (
-          <Button
-            className="mt-5 w-full"
-            key={teacher.pageUrl}
-            type="default"
-            href={teacher.pageUrl}
-            target="_blank"
-          >
-            {teacher.pageTitle || "דף פרופיל"}
-          </Button>
-        )}
-      </Card>}
-    </div>
+    <>
+      {teacher &&
+        <Card
+          hoverable
+          className="bio-card"
+        >
+          <div className="bio-card-content">
+            {imgComponent}
+            <Title level={3} className="bio-card-title">
+              {teacher.fullName}
+            </Title>
+            <Text>{teacher.bio}</Text>
+            {teacher.pageUrl && (
+              <Button
+                className="mt-5 w-full"
+                key={teacher.pageUrl}
+                type="default"
+                href={teacher.pageUrl}
+                target="_blank"
+              >
+                {teacher.pageTitle || "דף פרופיל"}
+              </Button>
+            )}
+          </div>
+        </Card>
+      }
+    </>
   );
 }
 
