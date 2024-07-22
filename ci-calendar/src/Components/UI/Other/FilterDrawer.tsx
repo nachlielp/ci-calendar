e4d599f8feee4865eba9e27218f257eedd930299
Nachliel <nachpok@gmail.com>
2024-07-22 12:52:05 +0300

    Update FilterDrawer.tsx

--- ci-calendar/src/Components/UI/Other/FilterDrawer.tsx
+++ ci-calendar/src/Components/UI/Other/FilterDrawer.tsx
@@ -43,7 +43,7 @@ export default function FilterDrawer() {
                 className="filter-drawer custom-drawer"
                 open={modalOpen}
                 onClose={() => setModalOpen(false)}
-                height="550px"
+                height="440px"
                 placement="bottom"
                 footer={<Button onClick={clearAllSearchParams} className="footer-btn">נקה הכל</Button>}
             >
