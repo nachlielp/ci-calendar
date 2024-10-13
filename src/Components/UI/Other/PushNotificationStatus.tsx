41cb65ff34f77a0c4b26b7cb66c06e7a73debf81
Nachliel <nachpok@gmail.com>
2024-10-14 00:00:18 +0300

    notification wip

--- src/Components/UI/Other/PushNotificationStatus.tsx
+++ src/Components/UI/Other/PushNotificationStatus.tsx
@@ -1,40 +1,25 @@
-import { useEffect, useState } from "react"
 import useMessagingPermission from "../../../hooks/useMessagingPermission"
 import { Icon } from "./Icon"
 import NotificationsBlockedModal from "./NotificationsBlockedModal"
-import { utilService } from "../../../util/utilService"
-import { PushNotificationPromission } from "../../../util/interfaces"
 
 const PushNotificationButton = () => {
-    const [status, setStatus] = useState<PushNotificationPromission | null>(
-        null
-    )
-
-    const { checkPermissionsAndToken, requestPermission } =
-        useMessagingPermission()
-
-    useEffect(() => {
-        if (utilService.isFirstNotificationPermissionRequest()) {
-            setStatus("default")
-        } else {
-            checkPermissionsAndToken().then((status) => setStatus(status))
-        }
-    }, [])
+    const { requestPermission, permissionStatus } = useMessagingPermission()
 
     return (
         <section className="notification-status-container">
-            {(status === "default" || status === null) && (
+            {(permissionStatus === "default" || permissionStatus === null) && (
                 <div onClick={requestPermission}>
                     <Icon icon="add_alert" />
                 </div>
             )}
-
-            {status === "denied" && (
+            {permissionStatus === "denied" && (
                 <NotificationsBlockedModal
                     anchorElement={<Icon icon="notificationsOff" />}
                 />
             )}
-            {status === "granted" && <Icon icon="notifications_active" />}
+            {permissionStatus === "granted" && (
+                <Icon icon="notifications_active" />
+            )}
         </section>
     )
 }
--- src/hooks/useMessagingPermission.ts
+++ src/hooks/useMessagingPermission.ts
@@ -7,14 +7,16 @@ import { messaging } from "../firebase.messaging"
 import { PushNotificationPromission } from "../util/interfaces"
 
 export default function useMessagingPermission() {
-    const [notificationPermissionGranted, setNotificationPermissionGranted] =
-        useState<boolean>(false)
+    const [permissionStatus, setPermissionStatus] =
+        useState<PushNotificationPromission>(null)
 
     const { user } = useUser()
 
     // TODO: use local storage to check if the user has already granted permission
     useEffect(() => {
-        if (!utilService.isFirstNotificationPermissionRequest()) {
+        if (utilService.isFirstNotificationPermissionRequest()) {
+            setPermissionStatus("default")
+        } else {
             checkPermissionsAndToken()
         }
     }, [])
@@ -22,59 +24,57 @@ export default function useMessagingPermission() {
     const requestPermission = async () => {
         const permission = await Notification.requestPermission()
         utilService.setFirstNotificationPermissionRequest(permission)
-        return permission
+        setPermissionStatus(permission)
     }
 
-    const checkPermissionsAndToken =
-        async (): Promise<PushNotificationPromission> => {
-            try {
-                if (!user) return null
-                const { permission } = Notification
-                utilService.setFirstNotificationPermissionRequest(permission)
-                if (permission === "granted") {
-                    setNotificationPermissionGranted(true)
-
-                    const token = await getToken(messaging, {
-                        vapidKey: import.meta.env
-                            .VITE_VAPID_PUBLIC_FIREBASE_KEY,
-                    })
-
-                    const deviceId = utilService.getDeviceId()
-
-                    const existingToken = user.push_notification_tokens?.find(
-                        (token) => token.device_id === deviceId
-                    )?.token
-
-                    console.log(
-                        "does need update: ",
-                        token && token !== existingToken
-                    )
-
-                    if (token && token !== existingToken) {
-                        await usersService.updateUser(user.user_id, {
-                            push_notification_tokens: [
-                                ...(user.push_notification_tokens || []),
-                                {
-                                    device_id: utilService.getDeviceId(),
-                                    token,
-                                    created_at: new Date().toISOString(),
-                                    is_pwa: utilService.isPWA(),
-                                },
-                            ],
-                        })
-                    }
-                } else {
-                    console.error("Permission not granted for Notification")
-                }
-                return permission
-            } catch (error) {
-                console.error("Error:", error)
-                return null
+    const checkPermissionsAndToken = async () => {
+        try {
+            if (!user) {
+                setPermissionStatus(null)
+                return
             }
+            const { permission } = Notification
+            setPermissionStatus(permission)
+            utilService.setFirstNotificationPermissionRequest(permission)
+            if (permission === "granted") {
+                const token = await getToken(messaging, {
+                    vapidKey: import.meta.env.VITE_VAPID_PUBLIC_FIREBASE_KEY,
+                })
+
+                const deviceId = utilService.getDeviceId()
+
+                const existingToken = user.push_notification_tokens?.find(
+                    (token) => token.device_id === deviceId
+                )?.token
+
+                console.log(
+                    "does need update: ",
+                    token && token !== existingToken
+                )
+
+                if (token && token !== existingToken) {
+                    await usersService.updateUser(user.user_id, {
+                        push_notification_tokens: [
+                            ...(user.push_notification_tokens || []),
+                            {
+                                device_id: utilService.getDeviceId(),
+                                token,
+                                created_at: new Date().toISOString(),
+                                is_pwa: utilService.isPWA(),
+                            },
+                        ],
+                    })
+                }
+            } else {
+                console.error("Permission not granted for Notification")
+            }
+        } catch (error) {
+            console.error("Error:", error)
         }
+    }
 
     return {
-        notificationPermissionGranted,
+        permissionStatus,
         requestPermission,
         checkPermissionsAndToken,
     }
