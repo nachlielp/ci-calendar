f334a835dff9b9563754cfd11dea7ba25d0de58e
Nachliel <nachpok@gmail.com>
2024-10-17 23:36:27 +0300

    Update FullEventCard.tsx

--- src/Components/UI/DisplayEvents/FullEventCard.tsx
+++ src/Components/UI/DisplayEvents/FullEventCard.tsx
@@ -213,7 +213,7 @@ export default function FullEventCard({
                         label=""
                         successLabel=""
                         icon={"map"}
-                        successIcon="check"
+                        successIcon=""
                         callback={() =>
                             utilService.openGoogleMaps(
                                 ci_event.address.place_id,
@@ -227,7 +227,7 @@ export default function FullEventCard({
                         label=""
                         successLabel=""
                         icon={utilService.isIos() ? "ios_share" : "share"}
-                        successIcon="check"
+                        successIcon=""
                         callback={() =>
                             utilService.handleShareEvent(
                                 ci_event.id,
