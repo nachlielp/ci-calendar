0a0b6516200d7c8e3a8227669efb042bd4cfa9f7
Nachliel <nachpok@gmail.com>
2024-10-23 15:26:37 +0300

    remove loader and return cloudanry widget

--- src/Components/UI/Other/Loading.tsx
+++ src/Components/UI/Other/Loading.tsx
@@ -1,12 +1,12 @@
-import { useRef } from "react"
+// import { useRef } from "react"
 
 export default function Loading() {
-    const randomLoaderId = useRef(Math.floor(Math.random() * 10) + 1)
+    // const randomLoaderId = useRef(Math.floor(Math.random() * 10) + 1)
     return (
         <div className="loading-component">
-            <div className={`loader-wrapper-${randomLoaderId.current}`}>
+            {/* <div className={`loader-wrapper-${randomLoaderId.current}`}>
                 <span className={"loader"}></span>
-            </div>
+            </div> */}
         </div>
     )
 }
