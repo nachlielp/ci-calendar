9fb08488a1ff28c4b19e95502a84f1029fe6fb18
Nachliel <nachpok@gmail.com>
2024-11-09 11:00:01 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,24 @@ import ResetPasswordRequest from "./Components/Auth/ResetPasswordRequest"
 import ResetPasswordPage from "./Components/Pages/RestPasswordPage"
 import Login from "./Components/Auth/Login"
 import Signup from "./Components/Auth/Signup"
-
 import EventsPage from "./Components/Pages/EventsPage"
-
+import Loading from "./Components/Common/Loading"
 import { PrivateRoutes } from "./Components/Auth/PrivateRoutes"
+import EventsPageSkeleton from "./Components/Events/Display/EventsPageSkeleton"
+import AppHeader from "./Components/Layout/AppHeader"
+import BackgroundTiles from "./Components/Layout/BackgroundTiles"
+import { useCIEvents } from "./Context/CIEventsContext"
+
 import { UserType } from "./util/interfaces"
 import { SpeedInsights } from "@vercel/speed-insights/react"
+
+import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+import timezone from "dayjs/plugin/timezone"
 
 const CreateEventsPage = lazy(
     () => import("./Components/Pages/CreateEventsPage")
 )
-
 const SupportPage = lazy(() => import("./Components/Pages/SupportPage"))
 const ManageSupportPage = lazy(
     () => import("./Components/Pages/ManageSupportPage")
@@ -26,17 +33,12 @@ const FiltersAndNotificationsPage = lazy(
 )
 const BioPage = lazy(() => import("./Components/Pages/BioPage"))
 const ManageUsersPage = lazy(() => import("./Components/Pages/ManageUsersPage"))
-
-import dayjs from "dayjs"
-import utc from "dayjs/plugin/utc"
-import timezone from "dayjs/plugin/timezone"
-import ManageAllEventsPage from "./Components/Pages/ManageAllEventsPage"
-import UserEventsListPage from "./Components/Pages/UserEventListPage"
-import Loading from "./Components/Common/Loading"
-import EventsPageSkeleton from "./Components/Events/Display/EventsPageSkeleton"
-import AppHeader from "./Components/Layout/AppHeader"
-import { useCIEvents } from "./Context/CIEventsContext"
-import BackgroundTiles from "./Components/Layout/BackgroundTiles"
+const ManageAllEventsPage = lazy(
+    () => import("./Components/Pages/ManageAllEventsPage")
+)
+const UserEventsListPage = lazy(
+    () => import("./Components/Pages/UserEventListPage")
+)
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
