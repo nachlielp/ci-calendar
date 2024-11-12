8cc02bdce7bf180d27be90f1c575ca32a9e8778d
Nachliel <nachpok@gmail.com>
2024-11-13 01:07:14 +0200

    Update FiltersAndNotificationsPage.tsx

--- src/Components/Pages/FiltersAndNotificationsPage.tsx
+++ src/Components/Pages/FiltersAndNotificationsPage.tsx
@@ -1,6 +1,6 @@
 import Card from "antd/es/card"
-import PushNotificationStatusButton from "../Common/PushNotificationStatusButton"
-import SubscribeToTeachers from "../Users/SubscribeToTeachers"
+// import PushNotificationStatusButton from "../Common/PushNotificationStatusButton"
+// import SubscribeToTeachers from "../Users/SubscribeToTeachers"
 import UserNotificationsList from "../Users/UserNotificationsList"
 
 export default function FiltersAndNotificationsPage() {
@@ -8,8 +8,8 @@ export default function FiltersAndNotificationsPage() {
         <div className="filter-and-notifications-page">
             <Card className="filter-section">
                 <h3>הרשמה להתראות על ארועים חדשים של מורים וארגונים</h3>
-                <PushNotificationStatusButton />
-                <SubscribeToTeachers />
+                {/* <PushNotificationStatusButton /> */}
+                {/* <SubscribeToTeachers /> */}
                 <UserNotificationsList />
             </Card>
         </div>
