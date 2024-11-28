608b0a97f2fa334929e1ab5f68f5ebbd9f798263
Nachliel <nachpok@gmail.com>
2024-11-28 02:00:48 +0200

    style form add section buttons

--- src/Components/Events/Forms/AddLinksForm.tsx
+++ src/Components/Events/Forms/AddLinksForm.tsx
@@ -7,73 +7,80 @@ import { Icon } from "../../Common/Icon"
 
 export default function AddLinksForm() {
     return (
-        <Form.List name="links">
-            {(fields, { add, remove }) => (
-                <>
-                    {fields.map(({ key, name, ...restField }) => (
-                        <Card className="link-card" key={key}>
-                            <Row gutter={10} align="middle">
-                                <Col md={24} xs={24}>
-                                    <Form.Item
-                                        {...restField}
-                                        className="link-form-item"
-                                        name={[name, "title"]}
-                                        rules={[
-                                            {
-                                                required: true,
-                                                message: "שדה חובה",
-                                            },
-                                        ]}
-                                    >
-                                        <Input placeholder="כותרת " />
-                                    </Form.Item>
-                                </Col>
-                            </Row>
-                            <Row gutter={10} align="middle">
-                                <Col md={24} xs={24}>
-                                    <Form.Item
-                                        {...restField}
-                                        className="link-form-item"
-                                        name={[name, "link"]}
-                                        rules={[
-                                            {
-                                                required: true,
-                                                message: "שדה חובה",
-                                            },
-                                            { type: "url", warningOnly: true },
-                                        ]}
-                                    >
-                                        <Input placeholder="קישור" />
-                                    </Form.Item>
-                                </Col>
-                            </Row>
+        <section className="add-links-form">
+            <Form.List name="links">
+                {(fields, { add, remove }) => (
+                    <>
+                        {fields.map(({ key, name, ...restField }) => (
+                            <Card className="link-card" key={key}>
+                                <Row gutter={10} align="middle">
+                                    <Col md={24} xs={24}>
+                                        <Form.Item
+                                            {...restField}
+                                            className="link-form-item"
+                                            name={[name, "title"]}
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: "שדה חובה",
+                                                },
+                                            ]}
+                                        >
+                                            <Input placeholder="כותרת " />
+                                        </Form.Item>
+                                    </Col>
+                                </Row>
+                                <Row gutter={10} align="middle">
+                                    <Col md={24} xs={24}>
+                                        <Form.Item
+                                            {...restField}
+                                            className="link-form-item"
+                                            name={[name, "link"]}
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: "שדה חובה",
+                                                },
+                                                {
+                                                    type: "url",
+                                                    warningOnly: true,
+                                                },
+                                            ]}
+                                        >
+                                            <Input placeholder="קישור" />
+                                        </Form.Item>
+                                    </Col>
+                                </Row>
 
-                            <div className="button-container">
-                                <button
-                                    className="add-button-50"
-                                    onClick={() => remove(name)}
-                                >
-                                    <Icon
-                                        icon="doNotDisturb"
-                                        title="הסרת קישור"
-                                    />
-                                </button>
-                            </div>
-                        </Card>
-                    ))}
-                    <div className="add-button-container">
-                        <button className="add-button" onClick={() => add()}>
-                            <span className="add-button-content">
-                                <Icon
-                                    icon="addCircle"
-                                    className="add-icon"
-                                    title="הוספת קישור"
-                                />
-                            </span>
-                        </button>
-                    </div>
-                </>
-            )}
-        </Form.List>
+                                <div className="remove-button-container">
+                                    <button
+                                        className="remove-button"
+                                        onClick={() => remove(name)}
+                                    >
+                                        <Icon icon="doNotDisturb" />
+                                        <span className="remove-button-label">
+                                            הסרת קישור
+                                        </span>
+                                    </button>
+                                </div>
+                            </Card>
+                        ))}
+                        <div className="add-button-container">
+                            <button
+                                className="add-button"
+                                onClick={() => add()}
+                            >
+                                <span className="add-button-content">
+                                    <Icon icon="addCircle" />
+                                    <span className="add-button-label">
+                                        הוספת קישור
+                                    </span>
+                                </span>
+                            </button>
+                        </div>
+                    </>
+                )}
+            </Form.List>
+        </section>
     )
 }
--- src/Components/Events/Forms/EventSegmentsForm.tsx
+++ src/Components/Events/Forms/EventSegmentsForm.tsx
@@ -17,119 +17,124 @@ export default function EventSegmentsForm({
     teachers,
 }: EventSegmentsFormProps) {
     return (
-        <Form.List name="segments">
-            {(segments, { add, remove }) => (
-                <>
-                    {segments.map(({ key, name }) => (
-                        <Card className="segment-card" key={key}>
-                            <Row gutter={10} align="middle">
-                                <Col md={24} xs={24}>
-                                    <Form.Item
-                                        className="full-width"
-                                        name={[name, "event-type"]}
-                                        rules={[
-                                            {
-                                                required: true,
-                                                message: "שדה חובה",
-                                            },
-                                        ]}
-                                    >
-                                        <Select
-                                            options={eventOptions}
-                                            placeholder="סוג האירוע"
-                                        />
-                                    </Form.Item>
-                                </Col>
-                            </Row>
-
-                            <Row gutter={10} align="middle">
-                                <Col md={24} xs={24}>
-                                    <Form.Item
-                                        name={[name, "event-time"]}
-                                        rules={[
-                                            {
-                                                required: true,
-                                                message: "שדה חובה",
-                                            },
-                                        ]}
-                                        className="full-width"
-                                    >
-                                        <TimePicker.RangePicker
-                                            placeholder={[
-                                                "שעת התחלה",
-                                                "שעת סיום",
-                                            ]}
-                                            minuteStep={5}
-                                            format="HH:mm"
-                                            changeOnScroll
-                                            needConfirm={false}
-                                        />
-                                    </Form.Item>
-                                </Col>
-                            </Row>
-                            <Row gutter={10} align="middle">
-                                <Col md={24} xs={24}>
-                                    <Form.Item
-                                        name={[name, "teachers"]}
-                                        className="full-width"
-                                    >
-                                        <Select
-                                            mode="tags"
+        <section className="event-segments-form">
+            <Form.List name="segments">
+                {(segments, { add, remove }) => (
+                    <>
+                        {segments.map(({ key, name }) => (
+                            <Card className="segment-card" key={key}>
+                                <Row gutter={10} align="middle">
+                                    <Col md={24} xs={24}>
+                                        <Form.Item
                                             className="full-width"
-                                            placeholder="מורים - ניתן להוסיף מורים שלא נמצאים ברשימה"
-                                            filterOption={(input, option) =>
-                                                (option?.label ?? "")
-                                                    .toLowerCase()
-                                                    .indexOf(
-                                                        input.toLowerCase()
-                                                    ) >= 0
-                                            }
-                                            options={teachers}
-                                        />
-                                    </Form.Item>
-                                </Col>
-                            </Row>
-                            <Row gutter={10} align="middle">
-                                <Col md={24} xs={24}>
-                                    <Form.Item
-                                        name={[name, "event-tags"]}
-                                        className="full-width"
-                                    >
-                                        <Select
-                                            options={tagOptions}
-                                            mode="multiple"
-                                            placeholder="תגיות"
-                                        />
-                                    </Form.Item>
-                                </Col>
-                            </Row>
+                                            name={[name, "event-type"]}
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: "שדה חובה",
+                                                },
+                                            ]}
+                                        >
+                                            <Select
+                                                options={eventOptions}
+                                                placeholder="סוג האירוע"
+                                            />
+                                        </Form.Item>
+                                    </Col>
+                                </Row>
 
-                            <div className="button-container">
-                                <button
-                                    className="add-button-50"
-                                    onClick={() => remove(name)}
-                                >
-                                    <Icon
-                                        icon="doNotDisturb"
-                                        title="הסר אירוע המשך"
-                                    />
-                                </button>
-                            </div>
-                        </Card>
-                    ))}
-                    <div className="add-button-container">
-                        <button
-                            className="add-button"
-                            onClick={() => {
-                                add()
-                                form.setFieldsValue({ newSubTeacher: "" })
-                            }}
-                        >
-                            <Icon icon="addCircle" title="הוספת ארוע המשך" />
-                        </button>
-                    </div>
-                </>
-            )}
-        </Form.List>
+                                <Row gutter={10} align="middle">
+                                    <Col md={24} xs={24}>
+                                        <Form.Item
+                                            name={[name, "event-time"]}
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: "שדה חובה",
+                                                },
+                                            ]}
+                                            className="full-width"
+                                        >
+                                            <TimePicker.RangePicker
+                                                placeholder={[
+                                                    "שעת התחלה",
+                                                    "שעת סיום",
+                                                ]}
+                                                minuteStep={5}
+                                                format="HH:mm"
+                                                changeOnScroll
+                                                needConfirm={false}
+                                            />
+                                        </Form.Item>
+                                    </Col>
+                                </Row>
+                                <Row gutter={10} align="middle">
+                                    <Col md={24} xs={24}>
+                                        <Form.Item
+                                            name={[name, "teachers"]}
+                                            className="full-width"
+                                        >
+                                            <Select
+                                                mode="tags"
+                                                className="full-width"
+                                                placeholder="מורים - ניתן להוסיף מורים שלא נמצאים ברשימה"
+                                                filterOption={(input, option) =>
+                                                    (option?.label ?? "")
+                                                        .toLowerCase()
+                                                        .indexOf(
+                                                            input.toLowerCase()
+                                                        ) >= 0
+                                                }
+                                                options={teachers}
+                                            />
+                                        </Form.Item>
+                                    </Col>
+                                </Row>
+                                <Row gutter={10} align="middle">
+                                    <Col md={24} xs={24}>
+                                        <Form.Item
+                                            name={[name, "event-tags"]}
+                                            className="full-width"
+                                        >
+                                            <Select
+                                                options={tagOptions}
+                                                mode="multiple"
+                                                placeholder="תגיות"
+                                            />
+                                        </Form.Item>
+                                    </Col>
+                                </Row>
+
+                                <div className="remove-button-container">
+                                    <button
+                                        className="remove-button"
+                                        onClick={() => remove(name)}
+                                    >
+                                        <Icon icon="doNotDisturb" />
+                                        <span className="remove-button-label">
+                                            הסר אירוע המשך
+                                        </span>
+                                    </button>
+                                </div>
+                            </Card>
+                        ))}
+                        <div className="add-button-container">
+                            <button
+                                className="add-button"
+                                onClick={() => {
+                                    add()
+                                    form.setFieldsValue({ newSubTeacher: "" })
+                                }}
+                            >
+                                <Icon icon="addCircle" />
+                                <span className="add-button-label">
+                                    הוספת אירוע המשך
+                                </span>
+                            </button>
+                        </div>
+                    </>
+                )}
+            </Form.List>
+        </section>
     )
 }
