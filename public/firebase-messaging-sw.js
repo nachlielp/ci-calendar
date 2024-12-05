5c746ae818f30d8666540e01c4c6a5e60a7aa0fc
Nachliel <nachpok@gmail.com>
2024-12-05 23:21:00 +0200

    version test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = (7.8).toString()
+const CACHE_VERSION = (7.9).toString()
 const CACHE_NAME = `ci-calendar-cache-v${CACHE_VERSION}`
 
 self.addEventListener("install", (event) => {
--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ export enum EventAction {
     recycle,
 }
 
-export const CACHE_VERSION = (7.8).toString()
+export const CACHE_VERSION = (7.9).toString()
 
 const App = () => {
     return (
