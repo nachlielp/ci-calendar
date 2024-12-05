a2251c1007ab678baf7b63fad6384824b4f05792
Nachliel <nachpok@gmail.com>
2024-12-05 12:37:45 +0200

    version test

--- src/Components/Events/Display/EventsList.tsx
+++ src/Components/Events/Display/EventsList.tsx
@@ -10,7 +10,7 @@ interface IEventsListProps {
     events: CIEvent[]
 }
 
-const CACHE_VERSION = (7.1).toString()
+const CACHE_VERSION = (7.2).toString()
 
 export default function EventsList({ events }: IEventsListProps) {
     const { eventId } = useParams<{ eventId: string }>()
