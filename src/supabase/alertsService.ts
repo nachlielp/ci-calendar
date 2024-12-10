8e57e924f27aac998c7b4e8f978dd937321abe1a
Nachliel <nachpok@gmail.com>
2024-12-10 17:21:18 +0200

    cleanup

--- src/supabase/alertsService.ts
+++ src/supabase/alertsService.ts
@@ -3,7 +3,6 @@ import { supabase } from "./client"
 
 export const alertsService = {
     setAlertViewed,
-    getAlertById,
     updateAlert,
 }
 
@@ -14,29 +13,6 @@ async function setAlertViewed(alertId: string) {
             .update({ viewed: true })
             .eq("id", alertId)
         if (error) throw error
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-async function getAlertById(alertId: string) {
-    try {
-        const { data, error } = await supabase
-            .from("alerts")
-            .select("*, ci_events!inner (title, start_date,segments)")
-            .eq("id", alertId)
-            .single()
-
-        if (error) throw error
-
-        const formattedData = {
-            ...data,
-            title: data?.ci_events?.title,
-            start_date: data?.ci_events?.start_date,
-            firstSegment: data?.ci_events?.segments[0],
-        }
-        delete formattedData.ci_events
-        return formattedData
     } catch (error) {
         console.error(error)
     }
