599d4b56176d0ba72375a2ff944834b5f69b9512
Nachliel <nachpok@gmail.com>
2024-12-15 02:30:51 +0200

    Update Loading.tsx

--- src/Components/Common/Loading.tsx
+++ src/Components/Common/Loading.tsx
@@ -1,9 +1,12 @@
 // import { useRef } from "react"
 
+import Spin from "antd/es/spin"
+
 export default function Loading() {
     // const randomLoaderId = useRef(Math.floor(Math.random() * 10) + 1)
     return (
         <div className="loading-component">
+            <Spin />
             {/* <div className={`loader-wrapper-9`}>
                 <span className={"loader"}></span>
             </div> */}
