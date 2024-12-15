b035a69adddafc09145f148b152a078021d1f35c
Nachliel <nachpok@gmail.com>
2024-12-15 11:48:54 +0200

    style spinner

--- src/Components/Common/Loading.tsx
+++ src/Components/Common/Loading.tsx
@@ -1,16 +1,10 @@
-// import { useRef } from "react"
-
 import Spin from "antd/es/spin"
 import "../../styles/loading.css"
 
 export default function Loading() {
-    // const randomLoaderId = useRef(Math.floor(Math.random() * 10) + 1)
     return (
         <div className="loading-component">
-            <Spin />
-            {/* <div className={`loader-wrapper-9`}>
-                <span className={"loader"}></span>
-            </div> */}
+            <Spin size="large" />
         </div>
     )
 }
--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,11 +1,26 @@
 import React from "react"
-import ConfigProvider from "antd/es/config-provider"
+import { ConfigProvider } from "antd"
+
 interface ProvidersProps {
     children: React.ReactNode
 }
 
 const Providers: React.FC<ProvidersProps> = ({ children }) => {
-    return <ConfigProvider direction="rtl">{children}</ConfigProvider>
+    return (
+        <ConfigProvider
+            direction="rtl"
+            theme={{
+                token: {
+                    colorPrimary: "#444444",
+                },
+                components: {
+                    Spin: { dotSizeLG: 60 },
+                },
+            }}
+        >
+            {children}
+        </ConfigProvider>
+    )
 }
 
 export default Providers
