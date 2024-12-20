98a70e2e5ecbcf52a0422006c18bd29d67a3d55d
Nachliel <nachpok@gmail.com>
2024-12-20 13:22:09 +0200

    loading screen with svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy, useEffect } from "react"
+import { Suspense, lazy } from "react"
 import { Routes, Route, useNavigate } from "react-router-dom"
 import "./styles/overrides.css"
 
