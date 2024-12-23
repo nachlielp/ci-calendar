f41bdd6a3ed3f51cc5437d4b103d2a0b1f7fe3c8
Nachliel <nachpok@gmail.com>
2024-12-23 16:03:02 +0200

    1.5.6 fix reloading privet routes issue

--- src/Components/Auth/PrivateRoutes.tsx
+++ src/Components/Auth/PrivateRoutes.tsx
@@ -3,24 +3,26 @@ import { Navigate, Outlet } from "react-router-dom"
 import { UserType } from "../../util/interfaces"
 import Loading from "../Common/Loading"
 import { store } from "../../Store/store"
+import { observer } from "mobx-react-lite"
 
 interface PrivateRoutesProps {
     requiredRoles?: UserType[]
 }
 
-export const PrivateRoutes: React.FC<PrivateRoutesProps> = ({
-    requiredRoles: requiredRoles,
-}) => {
-    if (store.isLoading) {
-        return <Loading />
-    }
+export const PrivateRoutes: React.FC<PrivateRoutesProps> = observer(
+    ({ requiredRoles: requiredRoles }) => {
+        if (store.isLoading) {
+            return <Loading />
+        }
 
-    if (!store.getUser) {
-        return <Navigate to="/login" />
-    }
-    if (requiredRoles && !requiredRoles.includes(store.getUser.user_type)) {
-        return <Navigate to="/" />
-    }
+        if (!store.getUser) {
+            return <Navigate to="/login" />
+        }
 
-    return <Outlet />
-}
+        if (requiredRoles && !requiredRoles.includes(store.getUser.user_type)) {
+            return <Navigate to="/" />
+        }
+
+        return <Outlet />
+    }
+)
