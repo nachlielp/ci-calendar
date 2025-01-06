e70ca0d5ef174eff65c5cd44d96e1c4e2b667400
Nachliel <nachpok@gmail.com>
2025-01-06 11:47:11 +0200

    fix issue with alerts cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,7 @@ export enum EventAction {
 export const CACHE_VERSION = "1.6.76"
 
 export const EMAIL_SUPPORT = "info@ci-events.org"
-export const PAYBOX_URL = "https://www.payboxapp.com"
+export const PAYBOX_URL = "https://payboxapp.page.link/32yzdN1D1gix4AR37"
 export const DATE_LIMIT = dayjs().add(1, "year").toDate()
 
 const DisableAppNotice = () => {
--- src/Components/Alerts/AlertsAnchorVM.ts
+++ src/Components/Alerts/AlertsAnchorVM.ts
@@ -14,7 +14,7 @@ class AlertsAnchorVM {
             reaction(
                 () => store.getAlerts,
                 () => {
-                    this.cleanup()
+                    // this.cleanup()
                     if (navigator.setAppBadge) {
                         navigator.setAppBadge(this.alertsCount)
                     }
@@ -60,23 +60,24 @@ class AlertsAnchorVM {
             .map((r) => r.id)
         const futureEventIds = store.getEvents.map((e) => e.id)
 
-        const alertIdsToClose = []
+        const alertIdsToClose = this.getAlerts // Only process unviewed alerts
+            .filter((a) => {
+                if (a.type === NotificationType.admin_response) {
+                    return !openAdminRequestIds.includes(a.id)
+                } else if (
+                    a.type === NotificationType.reminder ||
+                    a.type === NotificationType.subscription
+                ) {
+                    return (
+                        (a.ci_event_id &&
+                            !futureEventIds.includes(a.ci_event_id)) ||
+                        !a.ci_event_id
+                    )
+                }
+                return false
+            })
+            .map((a) => a.id)
 
-        for (let a of this.getAlerts) {
-            if (
-                a.type === NotificationType.admin_response &&
-                !openAdminRequestIds.includes(a.id)
-            ) {
-                alertIdsToClose.push(a.id)
-            } else if (
-                (a.type === NotificationType.reminder ||
-                    a.type === NotificationType.subscription) &&
-                ((a.ci_event_id && !futureEventIds.includes(a.ci_event_id)) ||
-                    !a.ci_event_id)
-            ) {
-                alertIdsToClose.push(a.id)
-            }
-        }
         Promise.all(
             alertIdsToClose.map((id) => store.updateAlert({ id, viewed: true }))
         )
