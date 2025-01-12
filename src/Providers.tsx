6274fc9c5dbe02e2eda954ec2fbac5b4dc7839dc
Nachliel <nachpok@gmail.com>
2025-01-12 23:55:10 +0200

    wip sentry

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,11 +1,14 @@
 import React from "react"
 import { ConfigProvider } from "antd"
+import * as Sentry from "@sentry/react"
+import { Routes } from "react-router"
 
 interface ProvidersProps {
     children: React.ReactNode
 }
 
 import { PHProvider } from "./Providers/PHProvider"
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes)
 
 const Providers: React.FC<ProvidersProps> = ({ children }) => {
     return (
@@ -20,7 +23,17 @@ const Providers: React.FC<ProvidersProps> = ({ children }) => {
                 },
             }}
         >
-            <PHProvider> {children}</PHProvider>
+            <PHProvider>
+                {" "}
+                {React.Children.map(children, (child) => {
+                    if (React.isValidElement(child) && child.type === Routes) {
+                        return (
+                            <SentryRoutes>{child.props.children}</SentryRoutes>
+                        )
+                    }
+                    return child
+                })}
+            </PHProvider>
         </ConfigProvider>
     )
 }
--- src/instrument.js
+++ src/instrument.js
@@ -1,53 +1,60 @@
 import { useEffect } from "react"
 import * as Sentry from "@sentry/react"
+import dontev from "dotenv"
+
 import {
     createRoutesFromChildren,
     matchRoutes,
     useLocation,
     useNavigationType,
 } from "react-router"
+import React from "react"
 
 // Immediately invoke Sentry initialization
 const initSentry = () => {
     const dsn = import.meta.env.VITE_SENTRY_DSN
-
-    if (!dsn) {
+    // console.log("__dsn: ", dsn)
+    if (!dsn || dsn === "null" || dsn === "undefined") {
         console.warn("No Sentry DSN found. Sentry will not be initialized.")
         return
     }
 
-    Sentry.init({
-        dsn,
-        enabled: true,
-        environment: import.meta.env.MODE,
-        integrations: [
-            Sentry.reactRouterV6BrowserTracingIntegration({
-                useEffect,
-                useLocation,
-                useNavigationType,
-                createRoutesFromChildren,
-                matchRoutes,
-            }),
-            Sentry.replayIntegration(),
-        ],
-        beforeSend(event) {
-            if (process.env.NODE_ENV === "development") {
-                console.log(
-                    "Test error sent to Sentry. Check your Sentry dashboard."
-                )
-            }
-            return event
-        },
-        debug: false,
-        tracesSampleRate: 1.0,
-        allowUrls: [window.location.origin, /^https:\/\/ci-events\.org/],
-        tracePropagationTargets: [
-            window.location.origin,
-            /^https:\/\/ci-events\.org/,
-        ],
-        replaysSessionSampleRate: 0.1,
-        replaysOnErrorSampleRate: 1.0,
-    })
+    try {
+        Sentry.init({
+            dsn,
+            enabled: true,
+            environment: import.meta.env.MODE,
+            integrations: [
+                Sentry.reactRouterV7BrowserTracingIntegration({
+                    useEffect: React.useEffect,
+                    useLocation,
+                    useNavigationType,
+                    createRoutesFromChildren,
+                    matchRoutes,
+                }),
+                Sentry.replayIntegration(),
+            ],
+            beforeSend(event) {
+                if (process.env.NODE_ENV === "development") {
+                    console.log(
+                        "Test error sent to Sentry. Check your Sentry dashboard."
+                    )
+                }
+                return event
+            },
+            debug: false,
+            tracesSampleRate: 1.0,
+            allowUrls: [window.location.origin, /^https:\/\/ci-events\.org/],
+            tracePropagationTargets: [
+                window.location.origin,
+                /^https:\/\/ci-events\.org/,
+            ],
+            replaysSessionSampleRate: 0.1,
+            replaysOnErrorSampleRate: 1.0,
+        })
+    } catch (error) {
+        console.error("Failed to initialize Sentry:", error)
+    }
 }
 
 // Initialize immediately
