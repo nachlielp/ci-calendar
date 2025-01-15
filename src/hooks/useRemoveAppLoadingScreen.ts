f71de0fe643fc4f6a643b5fcec921afafb3ba928
Nachliel <nachpok@gmail.com>
2025-01-15 18:01:33 +0200

    cleanup

--- src/hooks/useRemoveAppLoadingScreen.ts
+++ src/hooks/useRemoveAppLoadingScreen.ts
@@ -1,5 +1,5 @@
 import { useEffect } from "react"
-
+//Remove the loader set in the index.html when the app is loaded
 export const useRemoveAppLoadingScreen = () => {
     useEffect(() => {
         const loader = document.getElementById("initial-loader")
