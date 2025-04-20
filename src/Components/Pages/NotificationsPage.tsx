edb8eaf753480ef0c35a9686bc864ac9411c8eab
Nachliel <nachpok@gmail.com>
2025-04-20 22:06:21 +0300

    remove notification settings

--- src/Components/Pages/NotificationsPage.tsx
+++ src/Components/Pages/NotificationsPage.tsx
@@ -1,14 +1,10 @@
-import SubscribeToTeachers from "../Users/SubscribeToTeachers"
-// import UserNotificationsList from "../Users/UserNotificationsList"
 import { observer } from "mobx-react-lite"
-import NotificationSwitch from "../Users/NotificationSwitch"
-import PermissionsStatusNotice from "../Common/PermissionsStatusNotice"
 import "../../styles/notifications-page.scss"
 import { EMAIL_SUPPORT } from "../../App"
-// import NewsletterFilter from "../Users/NewsletterFilter"
 import { getTranslation } from "../../util/translations"
 import { store } from "../../Store/store"
 import NewsletterFilter from "../Users/NewsletterFilter"
+
 const NotificationsPage = observer(() => {
     const branch = import.meta.env.VITE_BRANCH
     return (
@@ -17,10 +13,6 @@ const NotificationsPage = observer(() => {
                 {getTranslation("manageNotifications", store.language)}
             </label>
             <section className="filter-section card">
-                <PermissionsStatusNotice />
-                <NotificationSwitch />
-                <SubscribeToTeachers />
-
                 {branch !== "prod" && <NewsletterFilter />}
                 {/* <UserNotificationsList /> */}
             </section>
